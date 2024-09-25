939fa12657ffbe20e88ed6c767c1e49eee03686b
PravyKralSlovenska <stefancikjakub1@gmail.com>
2024-09-25 18:56:21 +0200

    zajtra prerobim

--- dama.py
+++ dama.py
@@ -11,12 +11,15 @@ class Hrac:
     def __init__(self, meno, farba:tuple):
         self.meno = meno
         self.farba = farba
-        self.panacikovia = []
+        self.panacikovia = [1,1,1,1,1,1,1,1,1,1,1,1]
         self.inv = []
+        self.kliknuty_panacik = None
+
+    def __str__(self):
+        return f'Hrac: {self.meno}'
 
 hrac1 = Hrac('Michael', (255, 125, 0))
 hrac2 = Hrac('Ondrej', (255, 255, 0))
-# hrac2.inv = [1,1,1,1,1,1,1,1,1,1,] # 10 panacikov
 
 class Text:
     def __init__(self, text, position: tuple[int,int], size: int, farba: tuple[int,int,int]):
@@ -49,19 +52,28 @@ class Stvorec:
 
 class Panacik:
     def __init__(self, x: int,y: int, size: tuple[int,int], farba: tuple[int,int,int]):
-        self.position = position
+        self.x = x
+        self.y = y
         self.size = size
         self.farba = farba
+        self.mozne_pohyby = []
         self.is_clicked = False
 
     def __str__(self):
-        return f'Panacik: {x, y}'
+        return f'Panacik: {self.x, self.y}'
+    
+    def __repr__(self):
+        return f'Panacik: {self.x, self.y}'
 
     def draw(self):
-        pg.draw.ellipse(SCREEN, self.farba, ((x*100 +15,y*100+15), self.size))
+        pg.draw.ellipse(SCREEN, self.farba, ((self.x*100 +15,self.y*100+15), self.size))
 
-    def is_clicked(self):
+    def is_clicked_func(self):
         self.is_clicked = True if self.is_clicked == False else False
+
+    def pohyb(self, x:int, y:int):
+        self.x = x
+        self.y = y
 
 class Dama:
     ...
@@ -70,7 +82,10 @@ class Sachovnica:
     def __init__(self, n=8):
         self.running = True
         self.n = n
-
+        self.hrac_na_rade = hrac1
+        self.slovnik = {1: hrac1, 2: hrac2}
+        self.klik = 0
+        
         # metody
         self.vytvor_sachovnicu_a_maticu()
         self.vytvor_texty()
@@ -90,11 +105,9 @@ class Sachovnica:
                     # cierny stvorec
                     self.sachovnica.append(Stvorec((i * 100, j * 100), (100, 100), (123, 63, 0)))
                     if i < 3:
-                        self.matica[i].append(1)
-                        # hrac1.panacikovia.append(Panacik(i, j, (70,70), hrac1.farba))
+                        self.matica[i].append(Panacik(j, i, (70,70), hrac1.farba))
                     elif i >= self.n - 3:
-                        self.matica[i].append(2)
-                        # hrac2.panacikovia.append(Panacik(i, j, (70,70), hrac2.farba))
+                        self.matica[i].append(Panacik(j, i, (70,70), hrac2.farba))
                     else:
                         self.matica[i].append(0)
         
@@ -103,36 +116,106 @@ class Sachovnica:
 
     def vytvor_texty(self):
         self.texty = []
+        # 0. text
         self.texty.append(Text('DAMA', (WIDTH - 150, 25), 50, (255, 255, 255)))
+        # 1. text
         self.texty.append(Text(f'{hrac1.meno}: {12-len(hrac2.inv)}', (WIDTH - 190, 75), 40, hrac1.farba))
+        # 2. text
         self.texty.append(Text(f'{hrac2.meno}: {12-len(hrac1.inv)}', (WIDTH - 190, 115), 40, hrac2.farba))
+        # 3. text
         self.texty.append(Text('Na tahu: ', (WIDTH - 150, 155), 40, (255, 255, 255)))
-        self.texty.append(Text('hrac1', (WIDTH - 150, 195), 30, (255, 255, 255)))
+        # 4. text 
+        self.texty.append(Text(self.hrac_na_rade.meno, (WIDTH - 150, 195), 30, self.hrac_na_rade.farba))
+        # 5. text
         self.texty.append(Text('STATUS:', (WIDTH - 150, 225), 40, (255, 255, 255)))
+        # 6. text
         self.texty.append(Text('klikni na panacika', (WIDTH - 190, 265), 30, (255, 255, 255)))
+        # 7. text podpis autora :D
+        # self.texty.append(Text('Jakub J. Stefancik', (WIDTH - 190, HEIGHT-30), 30, (255, 255, 255)))
     
+    def print_matica(self):
+        for riadok in self.matica:
+            print(riadok)
+
+    # musi ist diagolanlne o 1
+    # moze preskocit panacika, a aj viac, ale musi byt miesto za nim volne
+    # ak je na konci sachovnice, tak sa zmeni na dama
+    def mozne_pohyby(self, x:int, y:int):
+        start_bod = (x,y)
+        manual = [(1,1), (-1,1), (1,-1), (-1,-1)]
+
+        try:
+            if self.hrac_na_rade == hrac1:
+                for i in range(4):
+                    if self.matica[y+1][x-1] == 0:
+                        self.matica[y+1][x-1] = 3
+                    elif self.matica[y+1][x+1] == 0:
+                        self.matica[y+1][x+1] = 3
+                    elif self.matica[y+1][x-1] == hrac2:
+                        if self.matica[y+2][x-2] == 0:
+                            self.matica[y+2][x-2] = 3
+                    elif self.matica[y+1][x+1] == hrac2:
+                        if self.matica[y+2][x+2] == 0:
+                            self.matica[y+2][x+2] = 3
+
+            elif self.hrac_na_rade == hrac2:
+                for i in range(4):
+                    if self.matica[y-1][x-1] == 0:
+                        self.matica[y-1][x-1] = 3
+                    elif self.matica[y-1][x+1] == 0:
+                        self.matica[y-1][x+1] = 3
+                    elif self.matica[y-1][x-1] == hrac1:
+                        if self.matica[y-2][x-2] == 0:
+                            self.matica[y-2][x-2] = 3
+                    elif self.matica[y-1][x+1] == hrac1:
+                        if self.matica[y-2][x+2] == 0:
+                            self.matica[y-2][x+2] = 3
+        except IndexError:
+            pass
+                    
+
+    def change_player(self):
+        self.hrac_na_rade = hrac2 if self.hrac_na_rade == hrac1 else hrac1
+        self.texty[4].update(self.hrac_na_rade.meno, self.hrac_na_rade.farba)
+        self.texty[6].update('klikni na panacika')
+        self.remove_mozne_pohyby_z_matice()
+
+    def remove_mozne_pohyby_z_matice(self):
+        for riadok in self.matica:
+            for i in range(len(riadok)):
+                if riadok[i] == 3:
+                    riadok[i] = 0
+
     def clicked(self, pos:tuple):
         x, y = pos
         x = x // 100
         y = y // 100
 
-        # kvoli chybovym hlaskam
-        try:
-            self.sachovnica[y + self.n * x].update_color((0, 255, 0)) # zmeni farbu stvorca na zeleno
-            
-        except:
-            print('Klikol si mimo sachovnice')
-            self.texty[6].update('klikol si mimo sachovnice')
-        
-    def debug(self):
-        print('debug')
-        for i in range(self.n):
-            for j in range(self.n):
-                print(self.matica[j][i], end=' ')
-            print()
+        if self.matica[x][y] == 0:
+            return False
 
-        print('hrac1: ', hrac1.panacikovia)
-        print('hrac2: ', hrac2.panacikovia)
+        if self.klik == 0:            
+            if self.matica[y][x].farba == self.hrac_na_rade.farba:
+                self.hrac_na_rade.kliknuty_panacik = self.matica[y][x]
+                self.texty[6].update('klikni na ciel')
+                self.mozne_pohyby(x, y)
+                self.klik += 1
+
+            else:
+                self.texty[6].update('klikni na svojho panacika')
+
+        
+        elif self.klik == 1:
+            if self.matica[y][x] == 3:
+                self.hrac_na_rade.kliknuty_panacik.pohyb(x,y)
+                self.change_player()
+
+            else:
+                self.remove_mozne_pohyby_z_matice()
+                self.texty[6].update('klikni na panacika')
+                self.klik = 0
+
+        self.print_matica()
 
     def run(self):
         while(self.running):
@@ -142,7 +225,6 @@ class Sachovnica:
                     self.running = False
                 elif event.type == pg.MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        # vykona sa funkcia, ktora zisti, ktory stvorec bol kliknuty, nasledovne sa vykreslia mozne miesta pohybu (sede kruhy)
                         self.clicked(pg.mouse.get_pos())
 
             # pozadie
@@ -154,30 +236,34 @@ class Sachovnica:
             for text in self.texty:
                 text.draw()
 
-            # texty update - hraci
-            # self.texty[4].update(f'{hrac1.meno}: {12-len(hrac2.inv)}', hrac1.farba)
-
             # panacikovia
             for i in range(self.n):
                 for j in range(self.n):
-                    if self.matica[j][i] == 1:
-                        pg.draw.ellipse(SCREEN, hrac1.farba, (i * 100 + 15, j * 100 + 15, 70,70)) #platno, farba, (x, y, sirka, vyska)
-                    elif self.matica[j][i] == 2:
-                        pg.draw.ellipse(SCREEN, hrac2.farba, (i * 100 + 15, j * 100 + 15, 70,70))
-                    elif self.matica[j][i] == 3:
-                        pg.draw.ellipse(SCREEN, (125,125,125), (i * 100 + 25, j * 100 + 25, 50,50))
+                    # try:
+                        if self.matica[i][j] == self.hrac_na_rade.kliknuty_panacik:
+                            pg.draw.ellipse(SCREEN, (255,255,255), (j * 100 + 10, i * 100 + 10, 80, 80))
+                            pg.draw.ellipse(SCREEN, self.matica[i][j].farba, (j * 100 + 15, i * 100 + 15, 70, 70))
 
-            # for panacik in hrac1.panacikovia:
-            #     print("dsa")
-            #     panacik.draw()
-            
-            # for panacik in hrac2.panacikovia:
-            #     panacik.draw()
+                        elif self.matica[i][j].farba == hrac1.farba:
+                            pg.draw.ellipse(SCREEN, hrac1.farba, (j * 100 + 15, i * 100 + 15, 70,70))
+
+                        elif self.matica[i][j].farba == hrac2.farba:
+                            pg.draw.ellipse(SCREEN, hrac2.farba, (j * 100 + 15, i * 100 + 15, 70,70))
+
+                        elif self.matica[i][j] == 3:
+                            pg.draw.ellipse(SCREEN, (125,125,125), (j * 100 + 25, i * 100 + 25, 50, 50))
+
+                        else:
+                            print("???")
+
+                    # except AttributeError:
+                    #     pass
+                    # finally:
+                    #     pass
 
             pg.display.flip()
 
 if __name__ == '__main__':
     sachovnica = Sachovnica(8)
     sachovnica.run()
-    # sachovnica.debug()
     pg.quit()
